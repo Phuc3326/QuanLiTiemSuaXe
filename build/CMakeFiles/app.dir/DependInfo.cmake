bbd0ef5e4ab45fb7f5efcd3b67d6a5eb587fd401
Phuc3326 <huynhhuuphuc3326@gmail.com>
2025-04-16 23:45:21 +0700

    push tệp thực thi

---
+++ build/CMakeFiles/app.dir/DependInfo.cmake
@@ -0,0 +1,47 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/msys64/home/Dell/Project/QuanLiTiemSuaXe/main.c" "CMakeFiles/app.dir/main.c.obj" "gcc" "CMakeFiles/app.dir/main.c.obj.d"
+  "C:/msys64/home/Dell/Project/QuanLiTiemSuaXe/modules/billing.c" "CMakeFiles/app.dir/modules/billing.c.obj" "gcc" "CMakeFiles/app.dir/modules/billing.c.obj.d"
+  "C:/msys64/home/Dell/Project/QuanLiTiemSuaXe/modules/customers.c" "CMakeFiles/app.dir/modules/customers.c.obj" "gcc" "CMakeFiles/app.dir/modules/customers.c.obj.d"
+  "C:/msys64/home/Dell/Project/QuanLiTiemSuaXe/modules/services.c" "CMakeFiles/app.dir/modules/services.c.obj" "gcc" "CMakeFiles/app.dir/modules/services.c.obj.d"
+  "C:/msys64/home/Dell/Project/QuanLiTiemSuaXe/ui/components/box.c" "CMakeFiles/app.dir/ui/components/box.c.obj" "gcc" "CMakeFiles/app.dir/ui/components/box.c.obj.d"
+  "C:/msys64/home/Dell/Project/QuanLiTiemSuaXe/ui/components/button.c" "CMakeFiles/app.dir/ui/components/button.c.obj" "gcc" "CMakeFiles/app.dir/ui/components/button.c.obj.d"
+  "C:/msys64/home/Dell/Project/QuanLiTiemSuaXe/ui/components/grid.c" "CMakeFiles/app.dir/ui/components/grid.c.obj" "gcc" "CMakeFiles/app.dir/ui/components/grid.c.obj.d"
+  "C:/msys64/home/Dell/Project/QuanLiTiemSuaXe/ui/components/label.c" "CMakeFiles/app.dir/ui/components/label.c.obj" "gcc" "CMakeFiles/app.dir/ui/components/label.c.obj.d"
+  "C:/msys64/home/Dell/Project/QuanLiTiemSuaXe/ui/components/notebook.c" "CMakeFiles/app.dir/ui/components/notebook.c.obj" "gcc" "CMakeFiles/app.dir/ui/components/notebook.c.obj.d"
+  "C:/msys64/home/Dell/Project/QuanLiTiemSuaXe/ui/components/page.c" "CMakeFiles/app.dir/ui/components/page.c.obj" "gcc" "CMakeFiles/app.dir/ui/components/page.c.obj.d"
+  "C:/msys64/home/Dell/Project/QuanLiTiemSuaXe/ui/components/scrolled.c" "CMakeFiles/app.dir/ui/components/scrolled.c.obj" "gcc" "CMakeFiles/app.dir/ui/components/scrolled.c.obj.d"
+  "C:/msys64/home/Dell/Project/QuanLiTiemSuaXe/ui/components/search.c" "CMakeFiles/app.dir/ui/components/search.c.obj" "gcc" "CMakeFiles/app.dir/ui/components/search.c.obj.d"
+  "C:/msys64/home/Dell/Project/QuanLiTiemSuaXe/ui/components/treeView.c" "CMakeFiles/app.dir/ui/components/treeView.c.obj" "gcc" "CMakeFiles/app.dir/ui/components/treeView.c.obj.d"
+  "C:/msys64/home/Dell/Project/QuanLiTiemSuaXe/ui/index.c" "CMakeFiles/app.dir/ui/index.c.obj" "gcc" "CMakeFiles/app.dir/ui/index.c.obj.d"
+  "C:/msys64/home/Dell/Project/QuanLiTiemSuaXe/ui/pages/customer.c" "CMakeFiles/app.dir/ui/pages/customer.c.obj" "gcc" "CMakeFiles/app.dir/ui/pages/customer.c.obj.d"
+  "C:/msys64/home/Dell/Project/QuanLiTiemSuaXe/ui/pages/payment.c" "CMakeFiles/app.dir/ui/pages/payment.c.obj" "gcc" "CMakeFiles/app.dir/ui/pages/payment.c.obj.d"
+  "C:/msys64/home/Dell/Project/QuanLiTiemSuaXe/ui/pages/service.c" "CMakeFiles/app.dir/ui/pages/service.c.obj" "gcc" "CMakeFiles/app.dir/ui/pages/service.c.obj.d"
+  "C:/msys64/home/Dell/Project/QuanLiTiemSuaXe/ui/pages/statistical.c" "CMakeFiles/app.dir/ui/pages/statistical.c.obj" "gcc" "CMakeFiles/app.dir/ui/pages/statistical.c.obj.d"
+  "C:/msys64/home/Dell/Project/QuanLiTiemSuaXe/ui/utils/freeMemory.c" "CMakeFiles/app.dir/ui/utils/freeMemory.c.obj" "gcc" "CMakeFiles/app.dir/ui/utils/freeMemory.c.obj.d"
+  "C:/msys64/home/Dell/Project/QuanLiTiemSuaXe/ui/utils/get_last_iter.c" "CMakeFiles/app.dir/ui/utils/get_last_iter.c.obj" "gcc" "CMakeFiles/app.dir/ui/utils/get_last_iter.c.obj.d"
+  "C:/msys64/home/Dell/Project/QuanLiTiemSuaXe/ui/utils/listStore.c" "CMakeFiles/app.dir/ui/utils/listStore.c.obj" "gcc" "CMakeFiles/app.dir/ui/utils/listStore.c.obj.d"
+  "C:/msys64/home/Dell/Project/QuanLiTiemSuaXe/ui/utils/search_in_model.c" "CMakeFiles/app.dir/ui/utils/search_in_model.c.obj" "gcc" "CMakeFiles/app.dir/ui/utils/search_in_model.c.obj.d"
+  "C:/msys64/home/Dell/Project/QuanLiTiemSuaXe/ui/utils/update_txt.c" "CMakeFiles/app.dir/ui/utils/update_txt.c.obj" "gcc" "CMakeFiles/app.dir/ui/utils/update_txt.c.obj.d"
+  "C:/msys64/home/Dell/Project/QuanLiTiemSuaXe/ui/utils/update_txt_bil.c" "CMakeFiles/app.dir/ui/utils/update_txt_bil.c.obj" "gcc" "CMakeFiles/app.dir/ui/utils/update_txt_bil.c.obj.d"
+  "C:/msys64/home/Dell/Project/QuanLiTiemSuaXe/ui/utils/update_txt_ser.c" "CMakeFiles/app.dir/ui/utils/update_txt_ser.c.obj" "gcc" "CMakeFiles/app.dir/ui/utils/update_txt_ser.c.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
